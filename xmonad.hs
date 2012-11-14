3618c11da5945efa8d9f338f39c1d6493802e75c
alex <alex@pdp7.net>
2012-11-14 12:44:57 +0100

    add smartBorders

--- xmonad.hs
+++ xmonad.hs
@@ -1,12 +1,13 @@
 import XMonad
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.NoBorders
 
 -- apt-get install xmonad libghc-xmonad-contrib-dev libghc-xmonad-dev suckless-tools
  
 main = xmonad defaultConfig
 	{
 		manageHook = manageDocks <+> manageHook defaultConfig,
-		layoutHook = avoidStruts  $  layoutHook defaultConfig,
+		layoutHook = smartBorders $ avoidStruts $ layoutHook defaultConfig,
 		modMask = mod4Mask,
 		borderWidth = 2,
 		focusFollowsMouse = False
