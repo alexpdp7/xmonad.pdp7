037a44a8f7541fb3983ade528e34b4cfa0ea81a5
alex <alex@pdp7.net>
2014-11-25 20:36:54 +0100

    Use desktopConfig instead of defaultConfig
    Fixes:
    
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=769947

--- xmonad.hs
+++ xmonad.hs
@@ -2,10 +2,11 @@ import XMonad
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.SetWMName
+import XMonad.Config.Desktop
 
 -- apt-get install xmonad libghc-xmonad-contrib-dev libghc-xmonad-dev suckless-tools
  
-main = xmonad defaultConfig
+main = xmonad desktopConfig
 	{
 		manageHook = manageDocks <+> manageHook defaultConfig,
 		layoutHook = smartBorders $ avoidStruts $ layoutHook defaultConfig,
